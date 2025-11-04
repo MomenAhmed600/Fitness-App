6ca70ea5a9aae3ff75716a6e5de913b6b5dd5178
MomenAhmed600 <momen1616@gmail.com>
2025-11-04 19:43:54 +0200

    update1

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-500">
+      <h1 className="text-6xl font-bold text-black">Tailwind Works! 🎉</h1>
+    </div>
+  )
+}
+
+export default App
