2c256561b75441df9224095cbeb9b2a139b9cfa9
MomenAhmed600 <momen1616@gmail.com>
2025-11-29 01:43:12 +0200

    updatev

--- src/scenes/home/index.tsx
+++ src/scenes/home/index.tsx
@@ -6,6 +6,7 @@ import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import SponsorRedBull from "@/assets/SponsorRedBull.png";
 import SponsorForbes from "@/assets/SponsorForbes.png";
 import SponsorFortune from "@/assets/SponsorFortune.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 
 type Props = {
   setSelectedPage: (value: SelectedPage) => void;
@@ -18,23 +19,57 @@ const Home = ({ setSelectedPage }: Props) => {
     id="home"
     className="gap-16 bg-gray-20 py-10 md:h-full md:pb-0">
         {/* IMAGE AND MAIN HEADER */}
-        <div>
+        <div className="md:flex mx-auto w-5/6 items-center justify-center md:h-5/6">
           {/* MAIN HEADER */}
-          <div>
+          <div className="z-10 mt-32 md:basis-3/5">
           {/* HEADINGS */}
           <div>
             <div>
               <div>
-                <img />
+                <img alt="home-pagen-text" src={HomePageText} />
               </div>
+            </div>
+
+
+            <p className="mt-8 text-sm">
+              Unrivaled Gym. Unparalleled Training Fitness Classes. World Class
+              Studios to get the Body Shapes That you Dream of.. Get Your Dream
+              Body Now.
+            </p>
+          </div>
+
+          {/* ACTIONS */}
+            <div>
+              <ActionButton setSelectedPage={setSelectedPage}>
+                Join Now
+              </ActionButton>
+              <AnchorLink
+              className="text-sm font-bold text-primary-500 underline hover:text-Secondary-500"
+              onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+              href={`#${SelectedPage.ContactUs}`}
+              >
+                <p>Learn More</p>
+              </AnchorLink>
             </div>
           </div>
 
-          </div>
-
           {/* IMAGE */}
-          <div></div>
+          <div>
+            <img alt="home-pageGraphic" src={HomePageGraphic} />
+          </div>
         </div>
+        {/* SPONSORS */}
+        {isAboveMediumScreens && (
+          <div>
+            <div>
+              <div>
+                <img alt="redbull-sponsor" src={SponsorRedBull} />
+                <img alt="sponsorforbes-sponsor" src={SponsorForbes} />
+                <img alt="sponsorfortune-sponsor" src={SponsorFortune} />
+              </div>
+            </div>
+          </div>
+        )}
     </section>
   )
 }
